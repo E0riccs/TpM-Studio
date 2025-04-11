7cb13f1cdd6b71aaa930c15ea3b0d1094a7247c5
E0zz1420 <github.muzv9@slmails.com>
2025-04-11 21:33:07 +0800

    :引入路由

--- backend/app/router.py
+++ backend/app/router.py
@@ -4,7 +4,9 @@
 from fastapi import APIRouter
 
 from backend.app.HelloWorld.api.router import v1 as HelloWorld_v1
+from backend.app.DataFile.api.router import v1 as DataFile_v1
 
 router = APIRouter()
 
 router.include_router(HelloWorld_v1)
+router.include_router(DataFile_v1)
